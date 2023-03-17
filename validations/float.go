5ed1907786a0451c9216991454790a9dbf8959fb
Carlos García Rosales <carlosgarcia_97gr@hotmail.com>
2023-03-16 23:43:22 -0500

    more validation functions

--- validations/float.go
+++ validations/float.go
@@ -1,41 +1,41 @@
 package validations
 
-func FloatIsPositive(value float64) bool {
+func FloatIsPositive[T float64](value T) bool {
 	return value > 0
 }
 
-func FloatIsNegative(value float64) bool {
+func FloatIsNegative[T float64](value T) bool {
 	return value < 0
 }
 
-func FloatIsZero(value float64) bool {
+func FloatIsZero[T float64](value T) bool {
 	return value == 0
 }
 
-func FloatIsNonZero(value float64) bool {
+func FloatIsNonZero[T float64](value T) bool {
 	return value != 0
 }
 
-func FloatIsInRange(value float64, min float64, max float64) bool {
+func FloatIsInRange[T float64](value T, min T, max T) bool {
 	return value >= min && value <= max
 }
 
-func FloatIsLessThan(value float64, max float64) bool {
+func FloatIsLessThan[T float64](value T, max T) bool {
 	return value < max
 }
 
-func FloatIsLessThanOrEqualTo(value float64, max float64) bool {
+func FloatIsLessThanOrEqualTo[T float64](value T, max T) bool {
 	return value <= max
 }
 
-func FloatIsGreaterThan(value float64, min float64) bool {
+func FloatIsGreaterThan[T float64](value T, min T) bool {
 	return value > min
 }
 
-func FloatIsGreaterThanOrEqualTo(value float64, min float64) bool {
+func FloatIsGreaterThanOrEqualTo[T float64](value T, min T) bool {
 	return value >= min
 }
 
-func FloatIsMultipleOf(value float64, multiple float64) bool {
+func FloatIsMultipleOf[T float64](value T, multiple T) bool {
 	return value/multiple == 0
 }
--- validations/strings.go
+++ validations/strings.go
@@ -1,6 +1,10 @@
 package validations
 
-import "regexp"
+import (
+	"net"
+	"net/url"
+	"regexp"
+)
 
 // emailRegex is a regular expression for validating email addresses.
 // It is based on the regex from the HTML5 specification.
@@ -21,4 +25,54 @@ func StringIsBlank(value string) bool {
 // StringIsValidEmail checks if a string is a valid email address.
 func StringIsValidEmail(email string) bool {
 	return emailRegex.MatchString(email)
+}
+
+// StringIsValidHostname checks if a string is a valid hostname.
+// The string must not be longer than 255 characters.
+// The string must not end with a dot.
+// The string must only consist of valid characters.
+// The string must resolve to at least one IP address.
+func StringIsValidHostname(s string) bool {
+	// Compile the regular expression.
+	// The regular expression must match the entire string.
+	re, err := regexp.Compile(`^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$`)
+	if err != nil {
+		return false
+	}
+	if len(s) > 255 {
+		return false
+	}
+	if s[len(s)-1] == '.' {
+		return false
+	}
+	if !re.MatchString(s) {
+		return false
+	}
+	// Lookup the IP addresses for the hostname.
+	ips, err := net.LookupIP(s)
+	if err != nil {
+		return false
+	}
+	if len(ips) == 0 {
+		return false
+	}
+	return true
+}
+
+// StringIsIPv4 checks if a string is a valid IPv4 address.
+func StringIsIPv4(s string) bool {
+	ip := net.ParseIP(s)
+	return ip != nil && ip.To4() != nil
+}
+
+// StringIsIPv6 checks if a string is a valid IPv6 address.
+func StringIsIPv6(s string) bool {
+	ip := net.ParseIP(s)
+	return ip != nil && ip.To4() == nil
+}
+
+// StringIsURL checks if a string is a valid URL.
+func StringIsURL(s string) bool {
+	u, err := url.Parse(s)
+	return err == nil && u.Scheme != "" && u.Host != ""
 }

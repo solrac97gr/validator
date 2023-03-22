008037983ab6f5973f68f993d11c97c383df096a
Carlos García Rosales <carlosgarcia_97gr@hotmail.com>
2023-03-21 23:09:02 -0500

    adding network validation for tcp and udp address

--- validations/network.go
+++ validations/network.go
@@ -45,6 +45,18 @@ var (
 	ErrInvalidFQDN = errors.New("invalid FQDN")
 	// ErrInvalidRFC952Hostname is returned when an RFC 952 hostname is invalid.
 	ErrInvalidRFC952Hostname = errors.New("invalid RFC 952 hostname")
+	// ErrInvalidTCP4Addr is returned when ValidateTCP4Addr is given an invalid TCPv4 address.
+	ErrInvalidTCP4Addr = errors.New("invalid TCPv4 address")
+	// ErrInvalidTCP6Addr is returned when ValidateTCP6Addr is given an invalid TCPv6 address.
+	ErrInvalidTCP6Addr = errors.New("invalid TCPv6 address")
+	// ErrInvalidTCPAddr is returned when ValidateTCPAddr is given an invalid TCP address.
+	ErrInvalidTCPAddr = errors.New("invalid TCP address")
+	// ErrInvalidUDP4Addr is returned when ValidateUDP4Addr is given an invalid UDPv4 address.
+	ErrInvalidUDP4Addr = errors.New("invalid UDPv4 address")
+	// ErrInvalidUDP6Addr is returned when ValidateUDP6Addr is given an invalid UDPv6 address.
+	ErrInvalidUDP6Addr = errors.New("invalid UDPv6 address")
+	// ErrInvalidUDPAddr is returned when ValidateUDPAddr is given an invalid UDP address.
+	ErrInvalidUDPAddr = errors.New("invalid UDP address")
 )
 
 // ValidateIPAddress validates an IPv4 or IPv6 address.
@@ -177,6 +189,54 @@ func ValidateRFC952(hostname string) error {
 	matched, err := regexp.MatchString(pattern, hostname)
 	if err != nil || !matched {
 		return ErrInvalidRFC952Hostname
+	}
+	return nil
+}
+
+// ValidateTCP4Addr checks if the given address is a valid TCPv4 address.
+func ValidateTCP4Addr(addr string) error {
+	if _, err := net.ResolveTCPAddr("tcp4", addr); err != nil {
+		return ErrInvalidTCP4Addr
+	}
+	return nil
+}
+
+// ValidateTCP6Addr checks if the given address is a valid TCPv6 address.
+func ValidateTCP6Addr(addr string) error {
+	if _, err := net.ResolveTCPAddr("tcp6", addr); err != nil {
+		return ErrInvalidTCP6Addr
+	}
+	return nil
+}
+
+// ValidateTCPAddr checks if the given address is a valid TCP address.
+func ValidateTCPAddr(addr string) error {
+	if _, err := net.ResolveTCPAddr("tcp", addr); err != nil {
+		return ErrInvalidTCPAddr
+	}
+	return nil
+}
+
+// ValidateUDP4Addr checks if the given address is a valid UDPv4 address.
+func ValidateUDP4Addr(addr string) error {
+	if _, err := net.ResolveUDPAddr("udp4", addr); err != nil {
+		return ErrInvalidUDP4Addr
+	}
+	return nil
+}
+
+// ValidateUDP6Addr checks if the given address is a valid UDPv6 address.
+func ValidateUDP6Addr(addr string) error {
+	if _, err := net.ResolveUDPAddr("udp6", addr); err != nil {
+		return ErrInvalidUDP6Addr
+	}
+	return nil
+}
+
+// ValidateUDPAddr checks if the given address is a valid UDP address.
+func ValidateUDPAddr(addr string) error {
+	if _, err := net.ResolveUDPAddr("udp", addr); err != nil {
+		return ErrInvalidUDPAddr
 	}
 	return nil
 }

0facc9d84672b1216a896b1348b5e7323563b925
Carlos García Rosales <carlosgarcia_97gr@hotmail.com>
2023-03-25 23:16:35 -0500

    starting with format validations

--- validations/format.go
+++ validations/format.go
@@ -3,13 +3,15 @@ package validations
 import (
 	"encoding/base64"
 	"errors"
+	"regexp"
 )
 
 var (
-	ErrInvalidBase64       = errors.New("invalid base64 encoding")
-	ErrInvalidBase64URL    = errors.New("invalid base64url encoding")
-	ErrInvalidBase64RawURL = errors.New("invalid base64rawurl encoding")
-	ErrInvalidBIC          = errors.New("invalid BIC")
+	ErrInvalidBase64           = errors.New("invalid base64 encoding")
+	ErrInvalidBase64URL        = errors.New("invalid base64url encoding")
+	ErrInvalidBase64RawURL     = errors.New("invalid base64rawurl encoding")
+	ErrInvalidBIC              = errors.New("invalid BIC")
+	ErrInvalidBCP47LanguageTag = errors.New("invalid BCP47 language tag")
 )
 
 // IsBase64 checks if the given string is a valid base64 encoding.
@@ -49,6 +51,17 @@ func IsBIC(str string) error {
 		if c < 'A' || c > 'Z' {
 			return ErrInvalidBIC
 		}
+	}
+	return nil
+}
+
+// IsBCP47LanguageTag checks if the given string is a valid BCP47 language tag.
+func IsBCP47LanguageTag(str string) error {
+	// This function checks for the syntax of the language tag, but doesn't validate the subtags against the IANA Language Subtag Registry.
+
+	r := regexp.MustCompile(`^[a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})*$`)
+	if !r.MatchString(str) {
+		return ErrInvalidBCP47LanguageTag
 	}
 	return nil
 }
